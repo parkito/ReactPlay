1e5d9d0183c3b57e96b5c543a2b8ab428ee58290
parkito <artyom-karnov@yandex.ru>
2018-07-17 03:10:19 +0300

    Add multy directory

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Article from './Article'
+
+function App() {
+    return (
+        <div>
+            <div style={{color: 'red'}}>
+                <h1> App name</h1>
+            </div>
+            <Article/>
+        </div>
+    )
+}
+
+export default App

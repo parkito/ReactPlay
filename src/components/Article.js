9c5cdba6df013d85c54bc47a8fde1bd36dce2f9a
parkito <artyom-karnov@yandex.ru>
2018-07-17 16:20:38 +0300

    Add logic on button

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-    const {article} = props;
-    const body = <section>{article.text}</section>;
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>Page created {new Date().toTimeString()}</h3>
-        </div>
-    )
+class Article extends Component {
+    state = {
+        isOpen: true
+    };
+
+    render() {
+        const {article} = this.props;
+        const body = this.state.isOpen && <section>{article.text}</section>;
+        return (
+            <div>
+                <h2>{article.title}</h2>
+                <button onClick={this.handleClick}>
+                    {this.state.isOpen ? 'Close' : 'Open'}
+                </button>
+
+                {body}
+                <h3>Page created {new Date().toTimeString()}</h3>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 }
 
 export default Article

3098eeff064f7a39225201dbb5b0467375ec1c90
parkito <artyom-karnov@yandex.ru>
2018-07-20 15:51:25 +0300

    Add new project

---
+++ src/React_Begginer/component/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from "../../Article_App/components/App";
+
+class StorePicker extends React.Component {
+
+    render() {
+        return <p>Hello</p>
+    }
+
+}
+
+export default StorePicker
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react'
-import {render} from 'react-dom'
-import App from './Article_App/components/App'
+// Article_App
+// import React from 'react'
+// import {render} from 'react-dom'
+// import App from './Article_App/components/App'
+//
+// render(<App/>, document.getElementById('root'));
 
-render(<App/>, document.getElementById('root'));
+
+// React_Beginner
+import React from 'react';
+import {render} from 'react-dom'
+// import App from './Article_App/components/App'
+import StorePicker from './React_Begginer/component/StorePicker'
+
+// render(<App/>, document.getElementById('root'));
+render(<StorePicker/>, document.getElementById('root'));

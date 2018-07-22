d2a2cbefa36e3828955c22be3df33658b1fbfe76
parkito <artyom-karnov@yandex.ru>
2018-07-22 16:25:36 +0300

    Add states'

--- src/React_Begginer/component/App.js
+++ src/React_Begginer/component/App.js
@@ -5,14 +5,32 @@ import Order from './Order'
 import '../css/style.css';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.addFish = this.addFish.bind(this);
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+
+    addFish(fish) {
+        const fishes = {...this.state.fishes}
+        const timestamp = Date.now();
+        fishes['fish-${timestamp}'] = fish;
+        // this.setState({fishes: fishes});
+        this.setState({fishes});
+        console.log(fishes);
+    }
+
     render() {
         return (
             <div className='catch-of-the-day'>
                 <div className='menu'>
-                    <Header tagline = 'Fresh sea food market'/>
+                    <Header tagline='Fresh sea food market'/>
                 </div>
                 <Order/>
-                <Inventory/>
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
